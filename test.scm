bdcb796ef827d2058f3c2eb0a60b7db14ae16fb3
valindo <valindo.godinho@gmail.com>
2015-07-02 22:14:39 +0530

    Finished ex.1.11a

--- test.scm
+++ test.scm
@@ -1,22 +1,5 @@
-(define (average x y) 
-  		(/ (+ x y) 2)
-)
-
-(define (square x) 
-  		(* x x)
-)
-
-(define (abs x )
-		(if (< x 0) 
-      		(- x) 
-        	x
-        )  
-)
-
-(define (improve guess x )
-  		(average guess (/ x guess))
-)
-
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) .001)
-)
+(define (fib n)
+	(cond	((= n 0) 0)
+    ((= n 1) 1)
+      (else
+        (+ (fib (- n 1)) (fib (- n 2))))))
