1d8a9623bc184f87834e32a4c7456441d8d20ce8
valindo <valindo.godinho@gmail.com>
2015-07-03 06:05:23 +0530

    Added ex1.17.scm

---
+++ ex1.17.scm
@@ -0,0 +1,11 @@
+(define (double x)
+  (+ x x))
+(define (halve x)
+  (/ x 2))
+(define (even? x)
+  (= (remainder x 2) 0))
+
+(define (* a b)
+  (cond ((= b 0) 0)
+      ((even? b)( double (* a (halve b))))
+      (else (+ a (* a (- b 1))))))
