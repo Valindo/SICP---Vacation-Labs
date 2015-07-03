3e61aa461622b3634de3239b4d491b77b44c8992
valindo <valindo.godinho@gmail.com>
2015-07-04 00:48:01 +0530

    Finished ex1.21

---
+++ ex1.21.scm
@@ -0,0 +1,14 @@
+(define (sd n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divdes? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divdes? a b)
+  (= (remainder b a) 0))
+
+(sd 199) ;199
+(sd 1999) ;1999
+(sd 19999) ;7
