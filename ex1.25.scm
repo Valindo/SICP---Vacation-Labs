f3d1a37680c76c2ccf3233bc3ef9588fd747a675
valindo <valindo.godinho@gmail.com>
2015-07-04 03:08:50 +0530

    Done exercise ex1.25

---
+++ ex1.25.scm
@@ -0,0 +1,3 @@
+; with fast-exp there is alot of time and resource consumption as compared to expo
+; this is because in expo we can perform computation without having to deal with 
+; numbers greater that the modulo
