f3fde12e8c0a79711e763cbe42945b34e4b629a5
valindo <valindo.godinho@gmail.com>
2015-07-04 21:25:16 +0530

    Finsihed ex1.16

--- ex1.16.scm
+++ ex1.16.scm
@@ -1,6 +1,6 @@
 (define (fast-exp b a n)
   (cond ((= n 0) a)
-        ((even? n) (fast-exp b (* a (square b)) (/ n 2)))
+        ((even? n) (fast-exp (square b) a (/ n 2)))
         (else (fast-exp b (* a b) (- n 1)))))
 
-;fucking double value
+; fixed it bitches :D
