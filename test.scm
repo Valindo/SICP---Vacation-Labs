aa721ed9bdd7f021b06bcde202e2a9510888f0ad
valindo <valindo.godinho@gmail.com>
2015-07-06 01:45:23 +0530

    Changed file extensions

--- test.scm
+++ test.scm
@@ -1,31 +1,16 @@
-;fermats test
-(define (expmod base exp m)
-  (cond ((= exp 0) 1)
-        ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
-        (else
-          (remainder (* base (expmod base (- exp 1) m)) m))))
+(define (cube x)
+  (* x x x))
 
-(define (fermat-test n)
-  (define (try-it a)  
-    (= (expmod a n n) a))
-  (try-it (+ 1 (random (- n 1)))))
+(define (identity x)
+  x)
 
-(define (fast-prime? n times)
-  (cond ((= times 0) true)
-        ((fermat-test n) (fast-prime? n (- times 1)))
-        (else false)))
+(define (inc n)
+  (+ n 1))
 
-;ex 1.22 modifictation
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a ) (sum term (next a) next b))))
 
-(define (report-time elapsed-time)
-  (display "***")
-  (display elapsed-time))
-
-(define (start-prime-test n start-time) 
-  (if (fast-prime? n 10) (report-time (- (runtime) start-time))))
-
-(define (timed-prime-test n)
-  (newline)
-  (display n)
-  (start-prime-test n (runtime)))
-
+(define (sum-of-cubes a b)
+   (sum identity a inc b)) 
