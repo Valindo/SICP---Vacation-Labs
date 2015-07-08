e9d4efb0fd385126e61a210ca56f8487b819fe27
valindo <valindo.godinho@gmail.com>
2015-07-08 08:39:11 +0530

    Missed out last statement

--- ex1.37.scm
+++ ex1.37.scm
@@ -10,4 +10,5 @@
 (define (cont-fact-iter i result)
   (if (= i 0)
       result
-      (cont-fact-iter (- i 1) (/ (n i) (+ (d i) result)))))
+      (cont-fact-iter (- i 1) (/ (n i) (+ (d i) result))))
+  (cont-fact-iter (- k 1) (/ (n k) (d k))))
